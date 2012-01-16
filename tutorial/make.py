bd74530426e7b58ee25e0d6ca15f4d8bd292e9eb
bill <bill.mill@gmail.com>
2012-01-16 16:28:08 -0500

    cleaning up the make script a bit

--- tutorial/make.py
+++ tutorial/make.py
@@ -4,13 +4,17 @@ from mako.template import Template
 from mako.lookup import TemplateLookup
 from sys import argv
 from os import unlink, mkdir, listdir, chdir, getcwd
-from os.path import join, isdir
+from os.path import join, isdir, split
 from shutil import copytree, rmtree
 from subprocess import Popen, PIPE
 from yaml import load
+from operator import itemgetter
 
 def clean():
-    for f in glob("*.html"): unlink(f)
+    if isdir("build/js"):
+        rmtree("build/js")
+
+    for f in glob("build/*.html"): unlink(f)
 
 def build():
     if not isdir("build"):
@@ -19,32 +23,28 @@ def build():
     def required(page, required_vars):
         for var in required_vars: page[var] = page.get(var, "")
 
-    cwd = getcwd()
-    chdir("chapters")
-    fileslist = listdir(".")
-    pages = range(len(fileslist))
-    toc = range(len(fileslist))
-    for filename in fileslist:
-        if not filename.endswith(".yaml"):
-            continue
-        page = load(file(filename))
-        page["filename"] = page["name"]+".html"
-        pages[page["order"]] = page
-        toc[page["order"]] = (page["title"], page["filename"])
-        required(page, ["code", "explain_before", "explain_after", "title", "hidden_code",
-                        "library"])
+    chapters = [load(file(c)) for c in glob("chapters/*.yaml")]
 
-    chdir(join(cwd, "build"))
+    pages = []
+    toc = []
+    for chapter in sorted(chapters, key=itemgetter('order')):
+        link = "%s.html" % chapter["name"]
+        chapter["outfilename"] = join("build", link)
+
+        pages.append(chapter)
+        toc.append((chapter["title"], link))
+
+        required(chapter, ["code", "explain_before", "explain_after", "title", "hidden_code",
+                           "library"])
+
     for i, page in enumerate(pages):
         page["prev"] = pages[i-1]["name"] if i > 0 else ""
         page["next"] = pages[i+1]["name"] if i+1 < len(pages) else ""
         page["toc"]  = toc
-        file(page["filename"], 'w').write(
-            Template(filename="../templates/template.mak",
-                     lookup=TemplateLookup(directories=['.'])).render(**page))
+        file(page["outfilename"], 'w').write(
+             Template(filename="templates/template.mak",
+                      lookup=TemplateLookup(directories=['.'])).render(**page))
 
-    if isdir("build/js"):
-        rmtree("build/js")
     copytree("js", "build/js")
 
 def deploy():
